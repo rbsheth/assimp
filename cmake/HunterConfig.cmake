b8a991494425902841d88f4cde25a62e85b62fa4
Rahul Sheth <rahul@snap.com>
2020-08-27 14:27:30 -0400

    try again 8

---
+++ cmake/HunterConfig.cmake
@@ -0,0 +1,3 @@
+hunter_config(RapidJSON VERSION 1.1.0-ce81bc9-p1
+                        URL https://github.com/cpp-pm/rapidjson/archive/v1.1.0-ce81bc9-p1.tar.gz
+                        SHA1 2fb99e1c64dcd6702a56efb1bea9341c67c1d6e3)
